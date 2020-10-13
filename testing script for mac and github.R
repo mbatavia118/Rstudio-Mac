0e4c2ad7b31e12100f09ff15c790388cf31a92a2
mbatavia118 <mbatavia@gmail.com>
2020-10-12 23:07:42 -0400

    finishing up the Coursera instructions

---
+++ testing script for mac and github.R
@@ -0,0 +1,10 @@
+#checking to make sure my mac has linked GitHub and RStudio
+#no packages needed
+
+#clear brain
+rm(list=ls())
+
+#follow instructions from Coursera
+print("This text is being added in RStudio Mac")
+print("This doesn't make a lot of sense")
+print("I hope it does soon.")
